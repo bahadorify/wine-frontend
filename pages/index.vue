f0cb22bfb1f2af1b7b09ef090d4390d266cdff8f
Bahador Najafiazar <bahador@omegamedia.no>
2021-02-18 17:32:06 +0100

    Add slider for year filtering

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,32 @@
   <div class="container">
     <div class="filters">
       <search-bar @searchInputChange="doSearch($event)" />
+      <div class="filter-year">
+        <b>before year</b>
+        <span>{{ minYear }}</span>
+        <div class="slider-year">
+          <span>{{ valueYear }}</span>
+          <input
+            type="range"
+            :min="minYear"
+            :max="maxYear"
+            v-model="valueYear"
+            step="1"
+            @change="handleYearChange"
+          />
+        </div>
+        <span>{{ maxYear }}</span>
+      </div>
     </div>
-    <wine-table :wines="wines"></wine-table>
+    <wine-table :wines="resultWines"></wine-table>
   </div>
 </template>
 
 <script>
 import SearchBar from '~/components/common/SearchBar.vue'
 import WineTable from '~/components/common/WineTable.vue'
+import _ from 'lodash'
+
 const getWines = () =>
   import('~/data/winery_dump.json').then((m) => m.default || m)
 
@@ -17,23 +35,37 @@ export default {
   components: { WineTable, SearchBar },
   data() {
     return {
-      wines: [],
+      resultWines: [],
+      valueYear: 2020,
+      searchTerm: '',
     }
   },
   async asyncData({ req }) {
     const wines = await getWines()
 
-    return { wines, winesAll: wines }
+    const minYear = _.minBy(wines, (w) => w.year).year
+    const maxYear = _.maxBy(wines, (w) => w.year).year
+
+    return { wines, winesAll: wines, resultWines: wines, minYear, maxYear }
   },
   methods: {
     doSearch(searchTerm) {
+      this.searchTerm = searchTerm
       if (searchTerm.length >= 3) {
-        this.wines = this.winesAll.filter((w) =>
+        this.resultWines = this.filteredWines.filter((w) =>
           w.name.toLowerCase().includes(searchTerm.toLowerCase())
         )
       } else {
-        this.wines = this.winesAll
+        this.resultWines = this.filteredWines
       }
+    },
+    handleYearChange() {
+      this.doSearch(this.searchTerm)
+    },
+  },
+  computed: {
+    filteredWines() {
+      return this.winesAll.filter((w) => w.year <= this.valueYear)
     },
   },
 }
@@ -45,5 +77,14 @@ export default {
 }
 .filters {
   padding: 20px 10px;
+}
+.filter-year {
+  margin-top: 10px;
+  display: flex;
+}
+.slider-year {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
